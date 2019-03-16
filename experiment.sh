597eb3be3d09d03c5e472f6e95ead05a72636531
monky <monkyusage@gmail.com>
2019-03-16 14:08:41 +0100

    Initial commit

---
+++ experiment.sh
@@ -0,0 +1,1 @@
+ludwig experiment --model_definition_file model_spec.yaml --data_csv ../input/heart.csv
---
+++ viz.sh
@@ -0,0 +1,1 @@
+ludwig visualize --visualization learning_curves --training_statistics ../results/experiment_run_1/training_statistics.json
